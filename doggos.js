1dc99849ecea27ff4f89349eeceed57f0c9cd988
Adam Nagy <an2208@nyu.edu>
2020-01-15 22:48:20 +0100

    added styling and loading anim

--- doggos.js
+++ doggos.js
@@ -1,11 +1,15 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
 const doggos = document.querySelector(".doggos");
+const loadingGif = document.querySelector("#loading-gif");
+
+loadingGif.style.visibility = "hidden";
 
 function addNewDoggo() {
   const promise = fetch(DOG_URL);
   promise
     .then(function(response) {
+      loadingGif.style.visibility = "visible";
       const processingPromise = response.json();
       return processingPromise;
     })
@@ -13,11 +17,11 @@ function addNewDoggo() {
       const img = document.createElement("img");
       img.src = processedResponse.message;
       img.alt = "Cute doggo";
+      loadingGif.style.visibility = "hidden";
       doggos.appendChild(img);
     });
 }
 
-document.querySelector(".add-doggo").addEventListener('click', addNewDoggo);
+document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
 
 console.log("this will log first");
-
