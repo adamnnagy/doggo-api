af1b0fc577892aa5688547835dc89b28facc483d
Adam Nagy <an2208@nyu.edu>
2020-01-16 10:07:30 +0100

    added animation to button

--- doggos.js
+++ doggos.js
@@ -24,4 +24,28 @@ function addNewDoggo() {
 
 document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
 
-console.log("this will log first");
+//animation
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('button');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
+  });
